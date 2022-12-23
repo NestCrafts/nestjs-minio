ca94973842daeb33f76b819cd834bae31d47672f
Rubin Bhandari <roobin.bhandari@gmail.com>
2022-12-23 10:25:24 +0545

    fix: export

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './nest-minio.connection.providers';
+export * from './nest-minio.decorator';
+export * from './nest-minio.module';
+export * from './nest-minio.options';
+export * from './nest-minio.service';
