8d39cf3676c207707ef3da72d40eb75e75f19dd4
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-07-29 23:48:05 +0545

    fix: isglobal option

--- src/nest-minio.module.ts
+++ src/nest-minio.module.ts
@@ -1,46 +1,18 @@
-import { DynamicModule, Module, OnApplicationShutdown, OnModuleInit } from '@nestjs/common';
+import {  Module, OnApplicationShutdown, OnModuleInit } from '@nestjs/common';
 import { NestMinioService } from './nest-minio.service';
-import { ConfigurableModuleClass } from './nest-minio.module-definition';
-import { createConnectionProvider } from './nest-minio.connection.providers';
-import { NestMinioOptions } from './nest-minio.options';
+import {  ConfigurableModuleClass, } from './nest-minio.module-definition';
 
 @Module({
 	providers: [NestMinioService],
 	exports: [NestMinioService],
 })
-export class NestMinioModule
-	extends ConfigurableModuleClass
-	implements OnModuleInit, OnApplicationShutdown
-{
-	static register(options: NestMinioOptions): DynamicModule {
-		const dynamicModule = super.register(options);
-		const connectionProvider = createConnectionProvider(options.name);
-
-		return {
-			...dynamicModule,
-			providers: [...(dynamicModule.providers ?? []), connectionProvider],
-			exports: [...(dynamicModule.exports ?? []), connectionProvider],
-		};
-	}
-
-	static registerAsync(options: any): DynamicModule {
-		const dynamicModule = super.registerAsync(options);
-		const connectionProvider = createConnectionProvider(options?.name);
-
-		return {
-			...dynamicModule,
-			providers: [...(dynamicModule.providers ?? []), connectionProvider],
-			exports: [...(dynamicModule.exports ?? []), connectionProvider],
-		};
-	}
-
-	constructor(readonly service: NestMinioService) {
+export class NestMinioModule extends ConfigurableModuleClass implements OnModuleInit,OnApplicationShutdown {
+		constructor(readonly service: NestMinioService) {
 		super();
 	}
 	async onModuleInit(): Promise<void> {
 		await this.service.checkConnection();
 	}
-
 	async onApplicationShutdown(): Promise<void> {
 		await this.service.disconnect();
 	}
